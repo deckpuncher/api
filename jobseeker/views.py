1a5c2ce5a3fcb9bc613be157b0352cdf441972a3
John Del Castillo <isicraigorshane@hotmail.com>
2017-04-18 18:52:49 +1000

    Added route configs, docstrings

--- jobseeker/views.py
+++ jobseeker/views.py
@@ -1,3 +1,7 @@
-from django.shortcuts import render
+""" this docstring will silence the warning """
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    """ default route view """
+    return HttpResponse("Hello, world. This is the index")
