f03b9fe733c42b6cf831a65b4a02533a07b02a2e
John Del Castillo <isicraigorshane@hotmail.com>
2017-04-18 19:14:08 +1000

    pause to put child to bed

--- jobseeker/models.py
+++ jobseeker/models.py
@@ -1,3 +1,17 @@
+""" Basic jobseeker object and profile sub resource """
 from django.db import models
 
 # Create your models here.
+class Jobseeker(models.Model):
+    """ Basic information about a jobseeker """
+    first_name = models.CharField(max_length=100, name="FirstName")
+    last_name = models.CharField(max_length=100, name="LastName")
+    email_address = models.CharField(max_length=200, name="Email")
+    dob = models.DateField('Date of Birth')
+
+class Profile(models.Model):
+    """ Jobseekers public facing profile """
+    jobseeker = models.ForeignKey(Jobseeker, on_delete=models.CASCADE)
+    about_me = models.CharField(max_length=1000, name="About Me")
+    
+
