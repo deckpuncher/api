c68e8d11f72d7539d2f49d92aff7ce31550d2429
John Del Castillo <isicraigorshane@hotmail.com>
2017-04-18 22:10:22 +1000

    Pluralised jobseeker

--- api/settings.py
+++ api/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'jobseeker.apps.JobseekerConfig',
+    'jobseekers.apps.JobseekersConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- api/urls.py
+++ api/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^jobseeker/', include('jobseeker.urls')),
+    url(r'^jobseekers/', include('jobseekers.urls')),
     url(r'^admin/', admin.site.urls),
 ]
